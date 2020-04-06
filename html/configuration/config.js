7896beb525fc738f0fcf89dfae3edc4136119422
jamessmith <james@cybersmith.io>
2020-04-06 09:03:16 -0400

    Created a vulnerable version of the web-login-site application

---
+++ html/configuration/config.js
@@ -0,0 +1,10 @@
+console.log('Loaded config.js');
+module.exports = {
+
+    //Cookie secret used for session cookie
+    cookieSecret: "m49sZXr02kgckqpmfhz63784nxasR8t4jFajvuE",
+
+	authProviders: {
+
+	}
+}
